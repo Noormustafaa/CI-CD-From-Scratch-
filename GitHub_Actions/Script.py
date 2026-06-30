b92e180063bfb424de2de85ed10aad5903b711a0
Noor Mustafa Rajpar <150359415+Noormustafaa@users.noreply.github.com>
2026-06-30 21:30:54 +0500

    Create Script.py

---
+++ GitHub_Actions/Script.py
@@ -0,0 +1,9 @@
+# script.py
+print("Hello! Yeh Python code GitHub Actions ke zariye chal raha hai.")
+
+# Ek simple calculation
+num1 = 5
+num2 = 10
+pured_sum = num1 + num2
+
+print(f"Result: {num1} + {num2} = {pured_sum}")
