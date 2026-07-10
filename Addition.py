8ce6be05e8ae4e2863d81f046660c13b4a979c88
Noor Mustafa Rajpar <150359415+Noormustafaa@users.noreply.github.com>
2026-07-10 18:55:50 +0500

    Added Wrong code

--- Addition.py
+++ Addition.py
@@ -1,6 +1,6 @@
 # 1. Aik function banaya jo do numbers ko add karke value RETURN karta hai
 def add_numbers(a, b):
-    return a + b
+    return a - b
 
 # 2. AUTOMATED TEST: Hum khud check kar rahe hain ke kya hamara function sahi kaam kar raha hai?
 expected_answer = 10
