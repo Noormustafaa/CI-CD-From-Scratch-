e4ec4720c3bf8bfa7168641a5c77ad6a3c42462a
Noor Mustafa Rajpar <150359415+Noormustafaa@users.noreply.github.com>
2026-07-10 18:36:21 +0500

    Added Python Program with Variable and Conditaions

---
+++ hello1.py
@@ -0,0 +1,12 @@
+# 1. Variables banayein
+student_naam = "Noor"
+marks = 85
+
+print(f"Student ka naam: {student_naam}")
+print(f"Marks: {marks}")
+
+# 2. Condition (If/Else) lagayein
+if marks >= 50:
+    print("Result: Pass! Mubarak ho 🎉")
+else:
+    print("Result: Fail! Aur mehnat ki zaroorat hai.")
